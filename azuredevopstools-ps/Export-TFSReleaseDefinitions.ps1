fcd5d6a52c982071e773adeeb9f8f9ac92f0eeeb
get-itips <andresg@capazero.net>
2019-05-17 14:26:14 -0300

    initial version

---
+++ azuredevopstools-ps/Export-TFSReleaseDefinitions.ps1
@@ -0,0 +1,38 @@
+$csvpath= "output.csv"
+$fqdn="tfs"
+$collectionName="collection"
+
+$credential=Get-Credential
+$csv = "Project;ReleaseDefinition`r`n"
+
+$WebSession = New-Object -TypeName Microsoft.PowerShell.Commands.WebRequestSession -Property @{Credentials=($credential)}
+
+$uriProjects="http://$fqdn/$collectionName/_apis/projects?api-version=1.0"
+
+$responseProjects=Invoke-RestMethod -WebSession $websession -Method GET -Uri $uriProjects
+
+foreach($project in $responseProjects.value.name)
+{
+
+$uri= "http://$fqdn/$collectionName/"+$project+"/_apis/release/definitions?api-version=2.2-preview.1"
+
+$response=Invoke-RestMethod -WebSession $websession -Method GET -Uri $uri
+
+$withoutRelDef=$true
+foreach($r in $response.value.name)
+{
+	
+	     $csv+=$project+";"+$r
+            $csv +="`r`n"
+$withoutRelDef=$false
+}
+
+if($withoutRelDef)
+{
+	     $csv+=$project+";NULL"
+            $csv +="`r`n"
+}
+}
+$csv
+$csv | Set-Content -Path $csvpath -Encoding utf8
+write-host "CSV File Saved to..." $csvpath
